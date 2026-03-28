9c31966a28ae1363031f96aa8067246bcac60f89
psh-infra <priyanshu911x@gmail.com>
2026-03-28 17:18:56 +0530

    new go files

---
+++ v01.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var s string priyanshu
+
+func main() {
+	fmt.Println(s)
+}
---
+++ variables01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func mod(x, y int) int {
+	return x % y
+}
+
+func main() {
+	fmt.Println(mod(7, 2))
+}
